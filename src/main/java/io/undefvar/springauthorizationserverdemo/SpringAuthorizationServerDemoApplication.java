a00ed3f587681aa6a4ec1b654fea379a4cebde01
Emmanuel Sahr Sewah <sewah2012@gmail.com>
2023-06-27 13:42:57 +0100

    intial commit

---
+++ src/main/java/io/undefvar/springauthorizationserverdemo/SpringAuthorizationServerDemoApplication.java
@@ -0,0 +1,13 @@
+package io.undefvar.springauthorizationserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthorizationServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthorizationServerDemoApplication.class, args);
+	}
+
+}
